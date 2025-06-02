1fd23ceced2dd085e7014c4f1a710948c5a79758
AlexandraSklyarova <alexandra.sklyarova09@gmail.com>
2025-06-02 10:32:13 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import altair as alt
+from vega_datasets import data
+
+cars = data.cars()
+
+scatter = alt.Chart(cars).mark_circle(size=60).encode(
+    x="Horsepower",
+    y="Miles_per_Gallon",
+    color="Origin",
+    tooltip=["Horsepower", "Miles_per_Gallon", "Origin"]
+).interactive()
+
+st.title("Altair Scatterplot in Streamlit")
+st.write("This scatterplot shows the relationship between Horsepower and MPG in cars.")
+st.altair_chart(scatter, use_container_width=True)
